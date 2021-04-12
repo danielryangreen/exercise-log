525ad8dbec843d306869cd04a58316fe6ffeb9fd
Dan Green <dan.ryan.green@gmail.com>
2021-04-12 14:07:41 -0600

    Combine exercises into workout model

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,35 +1,35 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const ExerciseSchema = new Schema({
-  type: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  name: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  duration: {
-    type: Number,
-    required: true
-  },
-  weight: {
-    type: Number,
-  },
-  reps: {
-    type: Number,
-  },
-  sets: {
-    type: Number,
-  },
-  distance: {
-    type: Number,
-  }
-});
+// const ExerciseSchema = new Schema({
+//   type: {
+//     type: String,
+//     trim: true,
+//     required: true
+//   },
+//   name: {
+//     type: String,
+//     trim: true,
+//     required: true
+//   },
+//   duration: {
+//     type: Number,
+//     required: true
+//   },
+//   weight: {
+//     type: Number,
+//   },
+//   reps: {
+//     type: Number,
+//   },
+//   sets: {
+//     type: Number,
+//   },
+//   distance: {
+//     type: Number,
+//   }
+// });
 
-const Exercise = mongoose.model("Exercise", ExerciseSchema);
+// const Exercise = mongoose.model("Exercise", ExerciseSchema);
 
-module.exports = Exercise;
+// module.exports = Exercise;
--- models/Workout.js
+++ models/Workout.js
@@ -6,10 +6,40 @@ const WorkoutSchema = new Schema({
     type: Date,
     default: Date.now
   },
+  // exercises: [
+  //   {
+  //     type: Schema.Types.ObjectId,
+  //     ref: "Exercise"
+  //   }
+  // ]
   exercises: [
     {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise"
+      type: {
+        type: String,
+        trim: true,
+        required: true
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: true
+      },
+      duration: {
+        type: Number,
+        required: true
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      }
     }
   ]
 });
